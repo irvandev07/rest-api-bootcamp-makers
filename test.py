f3f8e410e3332fb713b6241ddf1bc7ccf67f2578
Irvan <irvan.syachrialdih@gmail.com>
2022-05-20 16:40:20 +0700

    first update

--- test.py
+++ test.py
@@ -170,3 +170,11 @@ def handler():
     for x in range(len(req)):
         lst.append(str(findDigits(req[x])))
     return {'result': lst }
+
+def findDigits(n):
+    count = 0
+    print(list(str(n)))
+    for i in list(str(n)):
+        if int(i) != 0 and n % int(i) == 0:
+            count += 1
+    return count
